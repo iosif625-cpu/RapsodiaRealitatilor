c2f44837ca22ce31e397284c46b3ab99ed1d5aff
iosif625-cpu <iosif625@gmail.com>
2025-10-04 01:37:14 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,39 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("pdf-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/rapsodia-realitatilor-2025-octombrie.pdf",
-        "/manifest.json"
-      ]);
-    })
+const CACHE = "pdf-cache-v2";
+const ASSETS = [
+  "index.html",
+  "manifest.json",
+  "rapsodia-realitatilor-2025-octombrie.pdf",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.origin === self.location.origin) {
+    if (url.pathname.endsWith(".pdf")) {
+      event.respondWith(
+        caches.match(event.request).then((res) => res || fetch(event.request))
+      );
+      return;
+    }
+
+    event.respondWith(
+      caches.match(event.request).then((res) => res || fetch(event.request))
+    );
+  }
 });
